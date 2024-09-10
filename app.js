6cdfaa3edce1e292a3b8f88bc0a50e843ec07cdd
unknown <mohammed.modi@loginradius.com>
2024-09-10 22:53:55 +0530

    Imagekit code

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
+const express = require("express");
+const path = require("path");
+const indexRouter = require("./routes/index");
 
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// allow cross-origin requests
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Use the router for handling routes
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // Catch-all route for handling 404 errors
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
 // Serve the index.html file for the root route
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+router.get('/auth', (req, res) => {
+  const result = imagekit.getAuthenticationParameters();
+  res.send(result);
 });
 
 module.exports = router;
