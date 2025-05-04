0c53f96c820b819af36f1099d6fa11f36dbe44f4
Mohammed Modi <mohammed.modi@loginradius.com>
2025-05-04 17:11:00 +0530

    added dotenv dependecy

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const xlsx = require("xlsx");
-
+require("dotenv").config();
 const router = express.Router();
 
 const ImageKit = require("imagekit");
