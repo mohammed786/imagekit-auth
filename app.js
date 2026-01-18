b8906979190ae1364d688c5517aa37aa077367b3
Mohammed Modi <mohammed.modi@loginradius.com>
2026-01-18 13:17:44 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const indexRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
@@ -9,14 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // allow cross-origin requests
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 // Use the router for handling routes
 app.use("/", indexRouter);
